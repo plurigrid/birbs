021e131e9de0490b3b4c06e98f9e746a8662a3ed
Barton Rhodes <barton@bmorphism.xyz>
2022-08-16 02:14:23 -0700

    countercounterpoints

---
+++ counterpoints/android/app/src/main/kotlin/com/example/counterpoints/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.counterpoints
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- counterpoints/bin/counterpoints.dart
+++
@@ -1,5 +0,0 @@
-import 'package:counterpoints/counterpoints.dart' as counterpoints;
-
-void main(List<String> arguments) {
-  print('Hello world: ${counterpoints.calculate()}!');
-}
